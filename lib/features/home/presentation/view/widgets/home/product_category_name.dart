59f5fc9856acd9880c6a82e44f6301dcb1ad6bef
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-01-29 23:00:42 +0200

    add shimmer to product category name

--- lib/features/home/presentation/view/widgets/home/product_category_name.dart
+++ lib/features/home/presentation/view/widgets/home/product_category_name.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food/core/utils/app_color.dart';
 import 'package:food/core/utils/styles.dart';
 import 'package:food/features/home/presentation/view_model/category_cubit/category_cubit.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProductCategoryName extends StatefulWidget {
   const ProductCategoryName({super.key});
@@ -18,45 +19,83 @@ class _ProductCategoryNameState extends State<ProductCategoryName> {
   Widget build(BuildContext context) {
     return BlocBuilder<CategoryCubit, CategoryState>(
       builder: (context, state) {
-        return SliverToBoxAdapter(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: state is CategorySuccess
-                  ? List.generate(state.categoryModel.data.length, (index) {
-                      final cubit = state.categoryModel.data[index];
-                      return Padding(
-                        padding: currentIndex == 0
-                            ? EdgeInsets.only(left: 12.w)
-                            : EdgeInsets.only(right: 12.w),
-                        child: MaterialButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.r),
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              currentIndex = index;
-                            });
-                          },
-                          color: currentIndex == index
-                              ? AppColor.btn
-                              : AppColor.background,
-                          child: Text(
-                            cubit.name,
-                            style: Styles.s16_500.copyWith(
-                              color: currentIndex == index
-                                  ? Colors.white
-                                  : AppColor.textthirth,
-                            ),
+        switch (state) {
+          case CategoryInitial():
+            break;
+          case CategoryLoading():
+            return _buildShimmerCategore();
+          case CategorySuccess():
+            return SliverToBoxAdapter(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(state.categoryModel.data.length, (
+                    index,
+                  ) {
+                    final cubit = state.categoryModel.data[index];
+                    return Padding(
+                      padding: currentIndex == 0
+                          ? EdgeInsets.only(left: 12.w)
+                          : EdgeInsets.only(right: 12.w),
+                      child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.r),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        },
+                        color: currentIndex == index
+                            ? AppColor.btn
+                            : AppColor.background,
+                        child: Text(
+                          cubit.name,
+                          style: Styles.s16_500.copyWith(
+                            color: currentIndex == index
+                                ? Colors.white
+                                : AppColor.textthirth,
                           ),
                         ),
-                      );
-                    })
-                  : [],
-            ),
-          ),
-        );
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            );
+          case CategoryFailure():
+            return Center(child: Text(state.err));
+        }
+        return const SizedBox.shrink();
       },
     );
   }
+}
+
+Widget _buildShimmerCategore() {
+  return SliverToBoxAdapter(
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          4,
+          (index) => Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey.shade600,
+              highlightColor: Colors.grey.shade500,
+              child: Container(
+                width: 90.w,
+                height: 35.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
