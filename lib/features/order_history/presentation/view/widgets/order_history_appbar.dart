1a59f49bdb27b632e0b9fbbc480c70bcd4ddc58f
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-02-01 19:41:53 +0200

    edit appbar order history screen

--- lib/features/order_history/presentation/view/widgets/order_history_appbar.dart
+++ lib/features/order_history/presentation/view/widgets/order_history_appbar.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food/core/utils/app_color.dart';
 import 'package:food/features/order_history/presentation/view_model/cubit/order_history_cubit.dart';
 
 class OrderHistoryAppBar extends StatelessWidget
@@ -15,12 +15,35 @@ class OrderHistoryAppBar extends StatelessWidget
         children: [
           const Text("Order History"),
           const Spacer(),
-          Text(
-            "All Orders (${state.orderHistoryModel.data!.length})",
-            style: TextStyle(
-              fontSize: 12.sp,
-              color: Colors.grey,
-              fontWeight: FontWeight.bold,
+          SizedBox(
+            width: 40.w,
+            height: 40.h,
+            child: Stack(
+              children: [
+                Center(child: Icon(Icons.shopify_sharp, size: 34.sp)),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    width: 20.w,
+                    height: 20.h,
+                    decoration: BoxDecoration(
+                      color: AppColor.error,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "${state.orderHistoryModel.data?.length ?? 0}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
