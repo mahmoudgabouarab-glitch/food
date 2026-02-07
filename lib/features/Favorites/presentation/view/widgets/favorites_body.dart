9a7221ed336b08b642c068bd9b85e0d4e765ff4a
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-02-07 09:10:20 +0200

    creat fav title screen

--- lib/features/Favorites/presentation/view/widgets/favorites_body.dart
+++ lib/features/Favorites/presentation/view/widgets/favorites_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:food/features/Favorites/presentation/view/widgets/favorites_title.dart';
 
 class FavoritesBody extends StatelessWidget {
   const FavoritesBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Favorites"));
+    return Scaffold(
+      appBar: AppBar(centerTitle: false, title: const Text("Favorites")),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(children: [FavoritesTitle()]),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/Favorites/presentation/view/widgets/favorites_title.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:food/core/utils/app_color.dart';
+import 'package:food/core/utils/spacing.dart';
+import 'package:food/core/utils/styles.dart';
+
+class FavoritesTitle extends StatelessWidget {
+  const FavoritesTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Favorites", style: Styles.s18_600),
+        spaceH(10),
+        Divider(thickness: 3, color: AppColor.primary),
+        spaceH(10),
+        Row(
+          children: [
+            Text("1 items", style: Styles.s14_500),
+            Spacer(),
+            GestureDetector(
+              onTap: () {},
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Edit title", style: Styles.s14_500),
+                  spaceW(4),
+                  Icon(Icons.edit_note_outlined, color: AppColor.primary),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
