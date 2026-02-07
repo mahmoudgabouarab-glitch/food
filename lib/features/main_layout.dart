424b11eae464bf589e5ad4468798966c4774b26b
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-02-07 08:29:12 +0200

    add folder and files first screen to new featuer Favorites

--- lib/features/main_layout.dart
+++ lib/features/main_layout.dart
@@ -4,6 +4,7 @@ import 'package:food/core/network/servise_locator.dart';
 import 'package:food/core/utils/app_color.dart';
 import 'package:food/core/utils/function.dart';
 import 'package:food/core/widgets/custom_bar_layout.dart';
+import 'package:food/features/Favorites/presentation/view/favorites_view.dart';
 import 'package:food/features/profile/data/repo/profile_repo_impl.dart';
 import 'package:food/features/profile/presentation/view_model/profile/profile_cubit.dart';
 import 'package:food/features/cart/data/repo/cart_repo_impl.dart';
@@ -61,6 +62,11 @@ class _MainLayoutViewState extends State<_MainLayoutView> {
       screen: CartView(),
     ),
     MainTabItem(
+      label: 'Favorites',
+      icon: Icons.favorite,
+      screen: FavoritesView(),
+    ),
+    MainTabItem(
       label: 'History',
       icon: Icons.fastfood_outlined,
       screen: OrderHistoryView(),
@@ -79,7 +85,6 @@ class _MainLayoutViewState extends State<_MainLayoutView> {
         children: _tabs.map((e) => e.screen).toList(),
       ),
       bottomNavigationBar: BottomNavigationBar(
-       
         currentIndex: _currentIndex,
         selectedFontSize: 13,
         unselectedFontSize: 13,
